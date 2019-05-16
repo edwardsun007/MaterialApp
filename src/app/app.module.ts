4c8f4a29c69172c0568b9075126e4d0acf17d58d
edwardsun007 <edwardsun007@gmail.com>
2019-05-16 15:09:44 -0700

    installed flexlayout start integration on signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,37 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FlexLayoutModule } from "@angular/flex-layout";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { MaterialModule } from "./material.module"; // import the angular module that only needed for this app
 import { matchesElement } from "@angular/animations/browser/src/render/shared";
-import { BankAccountComponent } from './bank-account/bank-account.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { LoginComponent } from './auth/login/login.component';
-import { TrainingComponent } from './training/training.component';
-import { CurrentTrainingComponent } from './training/current-training/current-training.component';
-import { NewTrainingComponent } from './training/new-training/new-training.component';
-import { PastTrainingComponent } from './training/past-training/past-training.component';
-import { LandingComponent } from './landing/landing.component';
+import { BankAccountComponent } from "./bank-account/bank-account.component";
+import { SignupComponent } from "./auth/signup/signup.component";
+import { LoginComponent } from "./auth/login/login.component";
+import { TrainingComponent } from "./training/training.component";
+import { CurrentTrainingComponent } from "./training/current-training/current-training.component";
+import { NewTrainingComponent } from "./training/new-training/new-training.component";
+import { PastTrainingComponent } from "./training/past-training/past-training.component";
+import { LandingComponent } from "./landing/landing.component";
 @NgModule({
-  declarations: [AppComponent, BankAccountComponent, SignupComponent, LoginComponent, TrainingComponent, CurrentTrainingComponent, NewTrainingComponent, PastTrainingComponent, LandingComponent],
+  declarations: [
+    AppComponent,
+    BankAccountComponent,
+    SignupComponent,
+    LoginComponent,
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingComponent,
+    LandingComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
