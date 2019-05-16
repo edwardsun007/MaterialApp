29a4c675f1b07880be1859fd5295777d44214360
edwardsun007 <edwardsun007@gmail.com>
2019-05-16 13:00:53 -0700

    created signup markup and some css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+// components
+import { LandingComponent } from "./landing/landing.component";
+import { SignupComponent } from "./auth/signup/signup.component";
+import { LoginComponent } from "./auth/login/login.component";
+import { TrainingComponent } from "./training/training.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: LandingComponent },
+  { path: "signup", component: SignupComponent },
+  { path: "login", component: LoginComponent },
+  { path: "training", component: TrainingComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)], // set the above routes under root
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from "@angular/core";
-import { MatButtonModule, MatIconModule } from "@angular/material";
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule
+} from "@angular/material";
 
 /* this is file for managing all Angular Material imported and exported that your app need*/
 // decorator NgModule tell angular that this class is angular module
 @NgModule({
-  imports: [MatButtonModule, MatIconModule],
-  exports: [MatButtonModule, MatIconModule]
+  imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
+  exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule]
 })
 export class MaterialModule {}
